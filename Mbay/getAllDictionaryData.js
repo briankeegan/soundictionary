154eb40e31efaadd6a66a116eaae39ddf9a17c4b
Brian Keegan <brian.morris.keegan@gmail.com>
2022-03-01 12:33:29 -0500

    Maybe fix it?

--- Mbay/getAllDictionaryData.js
+++ Mbay/getAllDictionaryData.js
@@ -15,23 +15,34 @@ const getSoundPath = (func) => {
   return string.substring(start, end);
 };
 
-const definition = definitions[0];
+const definition = definitions[3];
 
 const sampleSentenceShape = {
   mbay: '', // e.g. 'Ngōn-kó̰o̰-á àl̄ gìdɨ̀-á tɨ́.',
   english: '', // e.g.  His brother climbed on his back.
   wordSoundPath: '', // e.g. 'Sound/Mba386W.mp3'
 };
+const expressionShape = {
+  expressionMbay: '', // e.g. 'Ngōn-kó̰o̰-á àl̄ gìdɨ̀-á tɨ́.',
+  expressionEnglish: '', // e.g.  His brother climbed on his back.
+  sampleSentence: {...sampleSentenceShape}
+};
 
-const getSampleSentences = (type) => {
+const getSampleSentencesAndExpressions = (type, skipFirst) => {
   let currentSibling = type.nextElementSibling;
-  const sampleSentencesMbay = [];
+  let sampleSentencesMbay = [];
   while (currentSibling !== null && currentSibling.className !== 'type') {
     sampleSentencesMbay.push(currentSibling);
     currentSibling = currentSibling.nextElementSibling;
   }
 
-  return sampleSentencesMbay.map((mbay) => {
+  // skipFirst: Edge case around "will (future marker, 1/2 pers. of à)."
+  // because "à" has the class "sara-bagirmi-lang" its thinking its a sample sentence.
+  sampleSentencesMbay = skipFirst
+    ? sampleSentencesMbay.slice(1)
+    : sampleSentencesMbay;
+
+  const sampleAndExpressions = sampleSentencesMbay.map((mbay) => {
     const sampleCopy = { ...sampleSentenceShape };
     sampleCopy.mbay = mbay.innerText;
     sampleCopy.english = mbay.nextSibling.data.trim();
@@ -41,11 +52,35 @@ const getSampleSentences = (type) => {
     }
     return sampleCopy;
   });
+  let sampleSentences = []
+  let expressions = [];
+  for (let i = 0; i < sampleAndExpressions.length; i++) {
+    const currentSampleOrExpression = sampleAndExpressions[i];
+    // if the current sentence is 'Expr: '
+    if (currentSampleOrExpression && currentSampleOrExpression.mbay.includes('Expr:')) {
+      // Get the next two and push them to expressions
+      // and iterate twice
+      const expressionCopy = { ...expressionShape };
+      const expression = sampleAndExpressions[++i];
+      const expressionSampleSentence = sampleAndExpressions[++i];
+      expressionCopy.expressionMbay = expression.mbay;
+      expressionCopy.expressionEnglish = expression.english;
+      expressionCopy.sampleSentence = expressionSampleSentence;
+      expressions.push(expressionCopy);
+    } else {
+      sampleSentences.push(currentSampleOrExpression);
+    }
+  }
+  return [sampleSentences, expressions];
 };
 typesShape = {
   type: '', // e.g. VAU, Aux, AF etc
   translation: '', // e.g. climb, climb up; go up.
   sampleSentences: [], // Array of sampleSentenceShape
+};
+
+const getHasOpenParenthesisWithoutClosing = (wordString) => {
+  return wordString.includes('(') && !wordString.includes(')');
 };
 
 const getDefinitions = (definition) => {
@@ -55,7 +90,25 @@ const getDefinitions = (definition) => {
     const typeShapeCopy = { ...typesShape };
     typeShapeCopy.type = type.innerText.trim();
     typeShapeCopy.translation = type.nextSibling.data.trim();
-    typeShapeCopy.sampleSentences = getSampleSentences(type);
+    // Edge case for "will (future marker, 1/2 pers. of à)."
+    //  because "à" has the class "sara-bagirmi-lang" its thinking its a sample sentence.
+    // if there is an "(" but not a ")" that means there is a closing one,
+    //  and a mbay word in it
+    const hasOpenParenthesisWithoutClosing =
+      getHasOpenParenthesisWithoutClosing(typeShapeCopy.translation);
+    if (hasOpenParenthesisWithoutClosing) {
+      const opening = typeShapeCopy.translation;
+      const mbayWord = type.nextSibling.nextSibling.innerHTML.trim();
+      const closingParenthesis =
+        type.nextSibling.nextSibling.nextSibling.data.trim();
+      typeShapeCopy.translation = `${opening} ${mbayWord} ${closingParenthesis}`;
+    }
+    const [sampleSentences, expressions] = getSampleSentencesAndExpressions(
+      type,
+      hasOpenParenthesisWithoutClosing
+    );
+    typeShapeCopy.sampleSentences = sampleSentences;
+    typeShapeCopy.expressions = expressions;
     return typeShapeCopy;
   });
 };
@@ -63,8 +116,48 @@ const getDefinitions = (definition) => {
 const wordShape = {
   word: '', // e.g. àl̄
   wordSoundPath: '', // e.g. Sound/Mba386W.mp3
+  synonym: '', // e.g. (Syn: ànḛ̄)
+  originLanguage: '', // e.g. French
+  alternative: '', // e.g. [dà̰nà̰ŋ]
   definitions: [], // Array of typesShape
 };
+
+// (Syn: ànḛ̄)
+// [dà̰nà̰ŋ]
+// French
+
+const getSynonymOriginLanguageAlternative = (definition) => {
+  // sola => synonymOriginLanguageOrAlternative
+  let soloa = definition.querySelector('.word').lastChild
+  soloa = soloa && soloa.data
+  if (!soloa) {
+    return ''
+  }
+  soloa = soloa.trim()
+
+  let synonym = '' // (Syn: ànḛ̄)
+  let alternative = '' // [dà̰nà̰ŋ]
+  let originLanguage = ''// French
+
+  // Synonym
+  if (soloa.includes('(Syn:')) {
+    const startIndex = soloa.indexOf('(Syn:');
+    const endIndex = soloa.indexOf(')') + 1
+    // remove the (Syn: and the )
+    synonym = soloa.substring(startIndex + 5, endIndex - 1).trim()
+    soloa = soloa.substring(0, startIndex) + soloa.substring(endIndex);
+  }
+  // Alternative
+  if (soloa.includes('[')) {
+    const startIndex = soloa.indexOf('[');
+    const endIndex = soloa.indexOf(']') + 1
+    // remove the [ and ]
+    synonym = soloa.substring(startIndex + 1, endIndex - 1).trim()
+    soloa = soloa.substring(0, startIndex) + soloa.substring(endIndex);
+  }
+  originLanguage = soloa.trim()
+  return [synonym, originLanguage, alternative]
+}
 
 const getWord = (definition) => {
   const wordShapeCopy = { ...wordShape };
@@ -75,9 +168,26 @@ const getWord = (definition) => {
     wordShapeCopy.wordSoundPath = getSoundPath(wordSound.onclick);
   }
   wordShapeCopy.definitions = getDefinitions(definition);
+  const [synonym, originLanguage, alternative] = getSynonymOriginLanguageAlternative(definition)
+  // Add synonym, originLanguage, alternative if they exist
+  wordShapeCopy.synonym = synonym
+  wordShapeCopy.originLanguage = originLanguage
+  wordShapeCopy.alternative = alternative
   return wordShapeCopy;
 };
 
 const word = getWord(definition);
 
 console.log(word);
+
+// Expr: file:///Users/briankeegan/Documents/dictionary/soundDictionary/Mbay/oldwebsite/Mbay/Mbay3.htm
+// Idioms:(see file:///Users/briankeegan/Documents/dictionary/soundDictionary/Mbay/oldwebsite/Mbay/Mbay3.htm)
+
+// Issues:
+// <span class="sara-bagirmi-lang">  </span> 37
+// <span class="sara-bagirmi-lang"></span> 5
+
+// Look into 
+// (Syn: =dɔ̀-nàng,dà-nìng)
+// (Syn: ==dɔ̀-lūu)
+// Just see if they are there in original
